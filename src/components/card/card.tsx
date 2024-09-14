5f7a3c6880e5209bd481f6b8952c3b702406a27f
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-09-15 01:12:04 +0600

    сделал задание 1

---
+++ src/components/card/card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const suitsObj = {
+    diams: '♦',
+    hearts: '♥',
+    clubs: '♣',
+    spades: '♠',
+};
+
+interface Rank_Suit {
+    rank: string;
+    suit: keyof typeof suitsObj;
+}
+
+const Card: React.FC<Rank_Suit> = ({rank, suit}) => {
+    return (
+        <div>
+            <span className={`card rank-${rank.toLowerCase()} ${suit}`}>
+                <span className="rank">{rank}</span>
+                <span className="suit">{suitsObj[suit]}</span>
+            </span>
+        </div>
+    );
+};
+export default Card;
